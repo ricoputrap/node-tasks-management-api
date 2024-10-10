f869c6c0c466c4c1a1ec37311aead999a849c817
Rico Putra Pradana <pradanaricoputra@gmail.com>
2024-10-11 05:38:30 +0700

    feat: implement password hashing utility functions

---
+++ src/passwordHashing/index.ts
@@ -0,0 +1,23 @@
+import * as crypto from 'crypto';
+import { CRYPTO_ALGO, CRYPTO_IV, CRYPTO_KEY } from '../../config/constants';
+
+// createCipheriv & createDecipheriv()
+const algo = CRYPTO_ALGO;
+const key = Buffer.from(CRYPTO_KEY, 'hex');
+const iv = Buffer.from(CRYPTO_IV, 'hex');
+
+export const encrypt = (password: string) => {
+  const cipher = crypto.createCipheriv(algo, key, iv);
+  let encrypted = cipher.update(password, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  return encrypted;
+};
+
+export const decrypt = (encrypted: string) => {
+  const decipher = crypto.createDecipheriv(algo, key, iv);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+
+  return decrypted;
+}
